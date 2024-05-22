1a1d3e4f427575998dcc34387517316429bc3ca1
loveshadowfiend <60311364+loveshadowfiend@users.noreply.github.com>
2024-05-23 00:34:00 +1000

    test: cors fix

--- src/api/manga.ts
+++ src/api/manga.ts
@@ -1,6 +1,9 @@
 export const fetchMangaByTitle = async (title: string) => {
     const response = await fetch(
-        `https://www.api.mangadex.org/manga?title=${title}&includes[]=cover_art&order[relevance]=desc`
+        `https://www.api.mangadex.org/manga?title=${title}&includes[]=cover_art&order[relevance]=desc`,
+        {
+            mode: "no-cors",
+        }
     );
 
     const data = await response.json();
