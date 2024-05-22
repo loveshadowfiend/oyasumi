51690c658b675bdbc4a000b5447b442a979ba7e0
loveshadowfiend <60311364+loveshadowfiend@users.noreply.github.com>
2024-05-22 16:33:32 +1000

    fix: notfound on rootlayouts

---
+++ src/app/(default)/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex w-full h-screen items-center justify-center">
+            <p className="font-semibold">404 not found</p>
+        </div>
+    );
+}
